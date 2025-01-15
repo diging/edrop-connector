ea9e3a21d99a5b4163fd33e47fa526c9a7ba0bc0
Julia Damerow <jdamerow@asu.edu>
2025-01-14 21:29:37 -0500

    [story/gh-14] moved postgres env variables in env file loaded by supervisor; closes gh-14

--- edrop/settings.py
+++ edrop/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os, logging
+
+logger = logging.getLogger(__name__)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,7 +84,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'), )
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 
 DATABASES = {
     'default': {
