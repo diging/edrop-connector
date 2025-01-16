fd6ea7450782d792a27c23fdb75caca649001b4d
Julia Damerow <jdamerow@users.noreply.github.com>
2025-01-16 13:23:36 -0500

    Story/gh 7 (#24)
    * [story/gh-7] added django-APScheduler to run cron jobs
    
    * [story/gh-7] added cron job to supervisor

--- edrop/settings.py
+++ edrop/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'track'
+    'track',
+    "django_apscheduler",
 ]
 
 MIDDLEWARE = [
@@ -152,3 +153,4 @@ GBF_ITEM_NR = os.environ.get('GBF_ITEM_NR', "") #Fix for the correct one
 GBF_ITEM_QUANTITY = 1.0
 GBF_SHIPPING_METHOD = os.environ.get('GBF_SHIPPING_METHOD', "FedEx Ground")
 
+CRON_JOB_FREQUENCEY = "*/1" # Should run the GBG check job once a day
