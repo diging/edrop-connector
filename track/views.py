dedf5bcd21ab243b699c737428bf61c9204db0b2
Julia Damerow <jdamerow@asu.edu>
2025-01-23 09:17:10 -0500

    Added version to home page.

--- track/views.py
+++ track/views.py
@@ -5,5 +5,16 @@ logger = logging.getLogger(__name__)
 
 def index(request):
     template = "track/home.html"
-    context = {}
+    # if there is a version file (which there should be after deployment)
+    # we'll show this on the home page
+    try:
+        with open('edrop/static/VERSION.txt', 'r') as file:
+            context = {
+                'version': file.read()
+            }
+    except Exception as ex:
+        logger.error("Could not get version.")
+        logger.error(ex)
+        context = {}
+
     return render(request, template, context)
