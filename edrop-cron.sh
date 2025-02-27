069d3aef22073451e9b84af38102d3740d578925
Julian Ophals <jophals@asu.edu>
2025-02-27 13:00:02 -0700

    [bug/gh-71] made pinging host and port configurable

--- edrop-cron.sh
+++ edrop-cron.sh
@@ -5,6 +5,8 @@ DJANGODIR=/edrop                              # Django project directory
 NUM_WORKERS=3                                 # Number of Gunicorn workers
 DJANGO_SETTINGS_MODULE=edrop.settings         # Django settings module
 DJANGO_WSGI_MODULE=edrop.wsgi                 # WSGI module name
+URL=127.0.0.1                                 # URL to ping before starting cron job
+PORT=8000                                     # Port to ping before starting cron job
 
 echo "Starting $NAME as `whoami`"
 
@@ -15,7 +17,7 @@ export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 mkdir -p /edrop/logs
 
-while ! nc -z 127.0.0.1 8000; do echo "eDrop not responding"; sleep 5; done
+while ! nc -z $URL $PORT; do echo "eDrop not responding"; sleep 5; done
 echo "eDrop has started successfully"
 
 # start the cron job
