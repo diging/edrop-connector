83e8aa1a0a99b1ee00538b2160b9e007c4d1c35e
Julia Damerow <jdamerow@asu.edu>
2025-02-28 09:46:29 -0500

    [gh-17] added a comment explaining the script change

--- edrop-cron.sh
+++ edrop-cron.sh
@@ -15,16 +15,21 @@ export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 mkdir -p /edrop/logs
 
+# When a new container is being created, eDROP Connector takes a long time to startup because all dependencies
+# need to be installed first. The cron job, however, needs all dependencies to be installed before it can start up.
+# Since we know that once eDROP Connector is up and running, all dependencies are installed, we just wait till we
+# get a 200 response from the web app before starting the cron job.
+# Using the autorestart property of Supervisor does not seem to work when deployed in production, hence this work around.
 while true; do 
     sleep 5;
     http_response=$(curl -s -o /dev/null -I -w "%{http_code}\n" http://localhost:8000/$APP_ROOT);
     if [ $http_response == "200" ]; then
         break
     else
-        echo "eDrop not responding"
+        echo "eDROP Connector is not yet responding."
     fi
 done
-echo "eDrop has started successfully"
+echo "eDROP Connector has started successfully"
 
 # start the cron job
 python manage.py runapscheduler
